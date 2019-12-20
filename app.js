87ff22836616ccf3729af1adbb9caccae714dd08
U6088169 <matchatta.toy@student.mahidol.ac.th>
2019-12-20 15:29:10 +0700

    update

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors")
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testAPIRouter = require("./routes/testApi");
 var student = require("./routes/server");
 var app = express();
 
@@ -22,8 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/testApi", testAPIRouter);
 app.use("/student", student);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
